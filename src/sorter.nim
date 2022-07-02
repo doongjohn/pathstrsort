3b86ab34acd92c25aa5bc59fed1563c2c2c482bd
doongjohn <doongjohn@gmail.com>
2022-07-02 12:08:19 +0900

    update sorting algorithm

--- src/sorter.nim
+++ src/sorter.nim
@@ -18,36 +18,36 @@ proc sortPathsGroupDirFirst*(paths: var seq[string], pathSeperator: char) {.inli
   paths.sort do (a, b: string) -> int:
     let aSepCount = a.count(pathSeperator)
     let bSepCount = b.count(pathSeperator)
-    # group directories first
-    if aSepCount == 0 or bSepCount == 0:
-      if aSepCount == 0: return 1
-      if bSepCount == 0: return 0
-    else:
+    if aSepCount != 0 and bSepCount != 0:
       let aLastSep = a.rfind(pathSeperator)
       let bLastSep = b.rfind(pathSeperator)
       # group directories first
       if aLastSep != bLastSep:
         if a.startsWith(b, bLastSep + 1): return 0
         if b.startsWith(a, aLastSep + 1): return 1
-    # sort ascii
-    if a > b: return 1
-    if a < b: return 0
+    elif aSepCount == 0 and bSepCount == 0:
+      # sort ascii
+      if a > b: return 1
+      if a < b: return 0
+    # group directories first
+    if aSepCount == 0: return 1
+    if bSepCount == 0: return 0
 
 proc sortPathsGroupDirLast*(paths: var seq[string], pathSeperator: char) {.inline.} =
   paths.sort do (a, b: string) -> int:
     let aSepCount = a.count(pathSeperator)
     let bSepCount = b.count(pathSeperator)
-    # group directories last
-    if aSepCount == 0 or bSepCount == 0:
-      if aSepCount == 0: return 0
-      if bSepCount == 0: return 1
-    else:
+    if aSepCount != 0 and bSepCount != 0:
       let aLastSep = a.rfind(pathSeperator)
       let bLastSep = b.rfind(pathSeperator)
       # group directories last
       if aLastSep != bLastSep:
         if a.startsWith(b, bLastSep + 1): return 1
         if b.startsWith(a, aLastSep + 1): return 0
-    # sort ascii
-    if a > b: return 1
-    if a < b: return 0
+    elif aSepCount == 0 and bSepCount == 0:
+      # sort ascii
+      if a > b: return 1
+      if a < b: return 0
+    # group directories last
+    if aSepCount == 0: return 0
+    if bSepCount == 0: return 1
